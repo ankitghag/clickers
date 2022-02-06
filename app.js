64aca8ab81ae42940f6be0aa15cf1999e2c6e536
ankitghag <86104240+ankitghag@users.noreply.github.com>
2022-02-06 19:16:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ const deletes=require("./routes/user.delete");
 
 const db=require("./db_config");
 const app = express();
-const port = 3000;
+const port = process.env.PORT||3000;
 
 /*  var options = {
   config: {
